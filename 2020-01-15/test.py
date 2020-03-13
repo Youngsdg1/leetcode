0a4c787c21e4511959f058d87810310c63e975cc
노영지 <youngsdg1@gmail.com>
2020-03-13 13:07:55 +0900

    test

--- 2020-01-15/test.py
+++ 2020-01-15/test.py
@@ -1,13 +1,3 @@
 arr = [4, 2, 3, 0, 3, 1, 2]
 start = 5
 
-length, visited = len(arr), set()
-def dfs(i):
-        if 0<=i<N and i not in visited:
-            if arr[i]==0: return True
-            visited.add(i)
-            return dfs(i+arr[i]) or dfs(i-arr[i])
-        return False
-    return dfs(start)
-
-print(dfs(5))
